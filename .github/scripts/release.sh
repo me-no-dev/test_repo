77ffb2ac310c6c29c5a1f4ca6b831d32a0ad335d
me-no-dev <hristo@espressif.com>
2019-09-25 22:46:45 +0300

    Update release.sh

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 EVENT_JSON=`cat $GITHUB_EVENT_PATH`
 
+#GITHUB_REF=refs/tags/<tag> or refs/heads/<branch>
+
 echo "Event: $GITHUB_EVENT_NAME, Repo: $GITHUB_REPOSITORY, Path: $GITHUB_WORKSPACE"
 
 if [ $GITHUB_EVENT_NAME == "repository_dispatch" ]; then
