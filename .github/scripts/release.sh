7d57db162642a12757c084860e479e70c624e10b
Me No Dev <me-no-dev@users.noreply.github.com>
2019-09-25 22:04:28 +0300

    Create release.sh

---
+++ .github/scripts/release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+EVENT_JSON=`cat $GITHUB_EVENT_PATH`
+
+action=`echo $EVENT_JSON | jq -r '.action'`
+assets_url=`echo $EVENT_JSON | jq -r '.release.assets_url'`
+draft=`echo $EVENT_JSON | jq -r '.release.draft'`
+prerelease=`echo $EVENT_JSON | jq -r '.release.prerelease'`
+tag=`echo $EVENT_JSON | jq -r '.release.tag_name'`
+branch=`echo $EVENT_JSON | jq -r '.release.target_commitish'`
+id=`echo $EVENT_JSON | jq -r '.release.id'`
+
+echo "Repo: $GITHUB_REPOSITORY, Path: $GITHUB_WORKSPACE"
+echo "Event: $GITHUB_EVENT_NAME, Action: $action, Branch: $branch" 
+echo "Tag: $tag, Draft: $draft, Pre-Release: $prerelease" 
+echo "Assets: $assets_url" 
