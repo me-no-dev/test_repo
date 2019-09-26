16d335d3db7b0325044569b8897682df663cd0ed
me-no-dev <hristo@espressif.com>
2019-09-26 19:32:45 +0300

    Update release.sh

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -5,11 +5,32 @@ if [ ! $GITHUB_EVENT_NAME == "release" ]; then
     exit 1
 fi
 
+EVENT_JSON=`cat $GITHUB_EVENT_PATH`
+action=`echo $EVENT_JSON | jq -r '.action'`
+draft=`echo $EVENT_JSON | jq -r '.release.draft'`
+prerelease=`echo $EVENT_JSON | jq -r '.release.prerelease'`
+tag=`echo $EVENT_JSON | jq -r '.release.tag_name'`
+branch=`echo $EVENT_JSON | jq -r '.release.target_commitish'`
+release_id=`echo $EVENT_JSON | jq -r '.release.id'`
+
+echo "Event: $GITHUB_EVENT_NAME, Repo: $GITHUB_REPOSITORY, Path: $GITHUB_WORKSPACE, Ref: $GITHUB_REF"
+echo "Action: $action, Branch: $branch, ID: $release_id" 
+echo "Tag: $tag, Draft: $draft, Pre-Release: $prerelease"
+
+if [ $draft == "true" ]; then
+	echo "It's a draft release. Exiting now..."
+	exit 0
+fi
+
+if [ ! $action == "published" ]; then
+	echo "Wrong action '$action'. Exiting now..."
+	exit 0
+fi
 
 function git_upload_asset(){
     local name=$(basename "$1")
     local mime=$(file -b --mime-type "$1")
-    curl -k -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw+json" -H "Content-Type: $mime" --data "@$1" "https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$id/assets?name=$name"
+    curl -k -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw+json" -H "Content-Type: $mime" --data "@$1" "https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$release_id/assets?name=$name"
 }
 
 function git_upload_to_pages(){
@@ -43,28 +64,6 @@ function git_upload_to_pages(){
 
     echo "$data" | curl -s -k -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw+json" -X PUT --data @- "https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$path"
 }
-
-EVENT_JSON=`cat $GITHUB_EVENT_PATH`
-action=`echo $EVENT_JSON | jq -r '.action'`
-draft=`echo $EVENT_JSON | jq -r '.release.draft'`
-prerelease=`echo $EVENT_JSON | jq -r '.release.prerelease'`
-tag=`echo $EVENT_JSON | jq -r '.release.tag_name'`
-branch=`echo $EVENT_JSON | jq -r '.release.target_commitish'`
-id=`echo $EVENT_JSON | jq -r '.release.id'`
-
-echo "Event: $GITHUB_EVENT_NAME, Repo: $GITHUB_REPOSITORY, Path: $GITHUB_WORKSPACE, Ref: $GITHUB_REF"
-echo "Action: $action, Branch: $branch, ID: $id" 
-echo "Tag: $tag, Draft: $draft, Pre-Release: $prerelease"
-
-if [ $draft == "true" ]; then
-	echo "It's a draft release. Exiting now..."
-	exit 0
-fi
-
-if [ ! $action == "published" ]; then
-	echo "Wrong action '$action'. Exiting now..."
-	exit 0
-fi
 
 # good time to build the assets
 if [ $prerelease == "true" ]; then
