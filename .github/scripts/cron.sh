20c8dfbd4c49130ee6076dbd1e2e8a7578a288c0
me-no-dev <hristo@espressif.com>
2019-09-26 22:20:36 +0300

    Create cron.sh

---
+++ .github/scripts/cron.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! $GITHUB_EVENT_NAME == "schedule" ]; then
+    echo "Wrong event '$GITHUB_EVENT_NAME'!"
+    exit 1
+fi
+
+echo "Event: $GITHUB_EVENT_NAME, Repo: $GITHUB_REPOSITORY, Path: $GITHUB_WORKSPACE, Ref: $GITHUB_REF"
