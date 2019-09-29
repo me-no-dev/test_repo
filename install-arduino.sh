3dbb654030cbf851e2aeac1d97a20193070f3411
me-no-dev <hristo@espressif.com>
2019-09-29 12:06:02 +0300

    Update install-arduino.sh

--- install-arduino.sh
+++ install-arduino.sh
@@ -75,7 +75,7 @@ function build_sketch(){ # build_sketch <fqbn> <path-to-ino> [extra-options]
 	if [ "$OS_IS_WINDOWS" == "1" ]; then
 		local ctags_version=`ls "$ARDUINO_IDE_PATH/tools-builder/ctags/"`
 		local preprocessor_version=`ls "$ARDUINO_IDE_PATH/tools-builder/arduino-preprocessor/"`
-		win_opts=-prefs=runtime.tools.ctags.path="$ARDUINO_IDE_PATH/tools-builder/ctags/$ctags_version" -prefs=runtime.tools.arduino-preprocessor.path="$ARDUINO_IDE_PATH/tools-builder/arduino-preprocessor/$preprocessor_version"
+		win_opts='-prefs=runtime.tools.ctags.path="$ARDUINO_IDE_PATH/tools-builder/ctags/$ctags_version" -prefs=runtime.tools.arduino-preprocessor.path="$ARDUINO_IDE_PATH/tools-builder/arduino-preprocessor/$preprocessor_version"'
 	fi
 
 	mkdir -p "$ARDUINO_BUILD_DIR"
